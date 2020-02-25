f77c0b5e9a7cb12b2eab9fedfea73ea229f45573
Elio-yang <2925539854@qq.com>
2020-02-25 11:16:07 +0800

    new

---
+++ Hello.java
@@ -0,0 +1,12 @@
+/**
+ * Hello
+ * @author:ElioYang
+ * @version 20.02.25
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        String x="Hello world";
+        System.out.println(x);
+    }
+}
